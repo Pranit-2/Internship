efce046460214bea0326469dc435f72e4f936adf
Pranit-2 <ptari2203@gmail.com>
2021-06-07 00:48:10 +0530

    Styles the content in Side Drawer

--- src/TemporaryDrawer.js
+++ src/TemporaryDrawer.js
@@ -8,8 +8,10 @@ import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   list: {
     width: 250,
   },
@@ -19,30 +21,44 @@ const useStyles = makeStyles({
   body: {
     backgroundColor: 'white',
     minWidth: 400,
+    flexGrow: 1,
   },
-});
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 export default function TemporaryDrawer(props) {
   const classes = useStyles();
-  
+
   return (
 
     <div className={classes.body}>
 
+
       <React.Fragment key={'right'}>
 
         <Drawer onClose={props.close} classes={{ paperAnchorRight: classes.body }} anchor={'right'} open={true}>
+          Country Information
+        
 
-          <List>
-            {props.data.name && Object.keys(props.data).filter((key) => ['capital', 'name', 'population', 'region'].includes(key)).map((key) =>
-              <ListItem button key={props.data[key]}>
-                <ListItemText primary={`${key} : ${props.data[key]}`} />
-
-              </ListItem>
-            )}
-          </List>
-
+            <List>
+              {props.data.name && Object.keys(props.data).filter((key) => key !== 'flag' && (typeof props.data[key] === 'string' || typeof props.data[key] === 'number')).map((key) =>
+                <ListItem button key={props.data[key]}>
+                  <Grid item xs={12} sm={6}>
+                    <ListItemText primary={key} />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <ListItemText secondary={props.data[key]} />
+                  </Grid>
+                </ListItem>
+              )}
+            </List>
+            
         </Drawer>
+
       </React.Fragment>
 
     </div>
